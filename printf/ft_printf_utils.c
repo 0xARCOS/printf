8abcf0fd87dea02bef697dacccf17290ed23fa6a
Ariel <129072087+V1nt3r4@users.noreply.github.com>
2025-02-09 15:21:27 +0100

    Update ft_printf_utils.c

--- printf/ft_printf_utils.c
+++ printf/ft_printf_utils.c
@@ -53,19 +53,6 @@ void	ft_putnbr(int n, int *i)
 	ft_putchar((n % 10) + '0', i);
 }
 
-void	ft_puthex(unsigned long long n, char format, int *i)
-{
-	char	*base;
-
-	if (format == 'x')
-		base = "0123456789abcdef";
-	else
-		base = "0123456789ABCDEF";
-	if (n >= 16)
-		ft_puthex(n / 16, format, i);
-	ft_putchar(base[n % 16], i);
-}
-
 void	ft_putptr(unsigned long long ptr, int *i)
 {
 	if (ptr == 0)
